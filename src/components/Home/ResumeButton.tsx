7c30ed0dc566450b7132d19062dfaf2796a88dc0
Tushar Chauhan <tusharchauhan0912@gmail.com>
2026-03-25 11:04:32 +0530

    resumE

--- src/components/Home/ResumeButton.tsx
+++ src/components/Home/ResumeButton.tsx
@@ -9,7 +9,7 @@ export default function ResumeButton() {
   return (
     <button
       type="button"
-      onClick={() => {}}
+      onClick={() => window.open("/Tushar_CV_12310419_FINAL_UPDATED.pdf", "_blank")}
       className="skillsDoubleBorder !inline-flex flex-row items-center justify-center gap-2 px-2 py-[3px] text-xs smalll transition-all duration-200 hover:scale-[1.02] active:scale-[0.98] w-fit cursor-pointer"
     >
       <FileText
